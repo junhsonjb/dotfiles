79dfdc2ec79a4b419f6d47d75aaba2ded3757d8e
Junhson Jean-Baptiste <junhsonjb@junhsons-mbp.mynetworksettings.com>
2024-10-21 17:23:31 -0400

    rename options to match documentation

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -7,13 +7,13 @@ HISTSIZE=5000
 HISTFILE=~/.zsh_history
 SAVEHIST=5000
 HISTDUP=erase
-setopt appendhistory
-setopt sharehistory
-setopt incappendhistory
-setopt hist_ignore_all_dups
-setopt hist_save_no_dups
-setopt hist_ignore_dups
-setopt hist_find_no_dups
+setopt APPEND_HISTORY
+setopt SHARE_HISTORY
+setopt INC_APPEND_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_IGNORE_DUPS
+setopt HIST_FIND_NO_DUPS
 
 # vi mode
 bindkey -v
